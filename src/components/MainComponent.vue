d63fce9ef027a78ef2838af67fe40ecddc3f38f2
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-07-20 14:03:49 +0200

    ADD milestone 3

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -26,6 +26,14 @@ export default {
             else {
                 return 'https://flagicons.lipis.dev/flags/4x3/aq.svg'
             }
+        },
+        getVote(vote) {
+            return Math.floor(vote / 2);
+        },
+        getStar() {
+            if (vote > 4.5) {
+
+            }
         }
     }
 }
@@ -35,28 +43,48 @@ export default {
     <main>
         <h2>FILM</h2>
         <ul>
-            <li v-for="(movie, index) in store.movies" :key="index">
+            <li v-for="(movie, index) in  store.movies " :key="index">
+                Copertina:
+                <div>
+                    <img class="poster" :src="` https://image.tmdb.org/t/p/w342/${movie.poster_path} `" alt="">
+                </div>
                 Titolo: {{ movie.title }}
                 <br>
                 Titolo Originale: {{ movie.original_title }}
                 <br>
                 Lingua:
-                <img :src="getFlag(movie.original_language)" alt="">
+                <img class="flag" :src="getFlag(movie.original_language)" alt="">
                 <br>
-                Voto: {{ movie.vote_average }}
+                Voto: {{ getVote(movie.vote_average) }}
+                <template v-if="getVote(movie.vote_average) > 0">
+                    <span v-for="(star) in 5" :key="star">
+                        <i :class="star <= getVote(movie.vote_average) ? 'fa-solid fa-star' : 'fa-regular fa-star'"></i>
+                    </span>
+                </template>
+
 
             </li>
         </ul>
         <h2>SERIE TV</h2>
         <ul>
-            <li v-for="(serie, index) in store.series" :key="index">
+            <li v-for="( serie, index ) in  store.series " :key="index">
+                Copertina:
+                <div>
+                    <img class="poster" :src="` https://image.tmdb.org/t/p/w342/${serie.poster_path} `" alt="">
+                </div>
+
                 Titolo: {{ serie.name }}
                 <br>
                 Titolo Originale: {{ serie.original_name }}
                 <br>
-                Lingua: <img :src="getFlag(serie.original_language)" :alt="serie.original_language">
+                Lingua: <img class="flag" :src="getFlag(serie.original_language)" :alt="serie.original_language">
                 <br>
-                Voto: {{ serie.vote_average }}
+                Voto: {{ getVote(serie.vote_average) }}
+                <template v-if="getVote(serie.vote_average) > 0">
+                    <span v-for="(star) in 5" :key="star">
+                        <i :class="star <= getVote(serie.vote_average) ? 'fa-solid fa-star' : 'fa-regular fa-star'"></i>
+                    </span>
+                </template>
 
             </li>
         </ul>
@@ -64,7 +92,18 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-img {
+.poster {
+    width: 150px;
+    height: 150px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+}
+
+.flag {
     display: inline-block;
     width: 10px;
     height: 10px;
